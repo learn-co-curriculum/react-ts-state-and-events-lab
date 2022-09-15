5cba451322e4d78c93578dd36628b0f1af093747
Jerrica Bobadilla <jbobadilla27@gmail.com>
2022-09-15 12:32:09 -0700

    add to cart

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
+
 interface Props {
   name: string;
   category: string;
 }
 
 function Item({ name, category }: Props) {
+  const [inCart, setInCart] = useState(false);
+  const itemClass = inCart ? "in-cart" : "";
+  const buttonText = inCart ? "Remove From Cart" : "Add to Cart";
+
+  function handleCart() {
+    setInCart((inCart) => !inCart);
+  }
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCart}>
+        {buttonText}
+      </button>
     </li>
   );
 }
